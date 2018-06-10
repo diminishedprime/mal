c58103f2ad1803889ea4784d5580037a5e349637
Matt Hamrick <matthewjhamrick@gmail.com>
2018-06-09 22:03:04 -0700

    fixed (sorta) previous steps.

--- my_rust/src/bin/step0_repl.rs
+++ my_rust/src/bin/step0_repl.rs
@@ -3,8 +3,7 @@ extern crate mal;
 fn main() {
     loop {
         mal::read::read("user> ")
-            .map(Ok::<String, String>)
-            .map(mal::print::print)
+            .map(|s| print!("{}", s))
             .expect("This should not happen");
         // .map(mal::print::print)
         // .expect("This should not happen");
--- my_rust/src/bin/step1_read_print.rs
+++ my_rust/src/bin/step1_read_print.rs
@@ -5,7 +5,6 @@ fn main() {
         mal::read::read("user> ")
             .map(mal::parser::parse)
             .map(mal::print::print)
-            .map(|_| println!())
             .expect("This should not happen");
         // .map(mal::print::print)
         // .expect("This should not happen");
